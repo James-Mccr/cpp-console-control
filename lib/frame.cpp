409f572212c82061aab523f9f3340fe3549cb596
James McCracken <James_Ian_Mccracken@protonmail.com>
2025-02-14 00:15:32 +0000

    Extended frame class to track delta time and improved limiter

--- lib/frame.cpp
+++ lib/frame.cpp
@@ -5,9 +5,32 @@
 Frame::Frame(long fps) 
 {
     milliseconds_per_frame = (1000 / fps);
+    limit_timestamp_ms = get_timestamp_ms();
+    delta_time_ms = 0;
+}
+
+long Frame::get_delta_time_ms()
+{
+    return delta_time_ms;
+}
+
+long Frame::get_milliseconds_per_frame()
+{
+    return milliseconds_per_frame;
 }
 
 void Frame::limit()
 {
-    std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds_per_frame));
+    long current_timestamp_ms = get_timestamp_ms();
+    delta_time_ms = current_timestamp_ms - limit_timestamp_ms;
+    if (delta_time_ms < milliseconds_per_frame)
+        std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds_per_frame-delta_time_ms));
+    limit_timestamp_ms = get_timestamp_ms();
 }
+
+long Frame::get_timestamp_ms()
+{
+    return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+}
+
+Frame frame {30};
--- lib/frame.h
+++ lib/frame.h
@@ -6,7 +6,15 @@ public:
     Frame(long fps);
     
     void limit();
+    long get_delta_time_ms();
+    long get_milliseconds_per_frame();
 
 private:
     long milliseconds_per_frame{};
+    long limit_timestamp_ms{};
+    long delta_time_ms{};
+
+    long get_timestamp_ms();
 };
+
+extern Frame frame;
